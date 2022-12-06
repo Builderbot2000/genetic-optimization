b72a8e06208f1e1a87bec02fa57d766e841c0db4
Kevin Tang <kta76@sfu.ca>
2022-12-05 22:44:05 -0800

    added optimizer

--- run_optimizer.py
+++ run_optimizer.py
@@ -1,7 +1,7 @@
-from selection_operators import *
-from crossover_operators import *
-from mutation_operators import *
-from fitness import evaluate_fitness
+import optimizer
 
 if __name__ == '__main__':
+    """Run experiments here"""
+    # Your implementation here
+    
     pass
