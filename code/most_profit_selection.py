e77f8927f45bce427a024caa100c2bbf8e765e57
Kevin Tang <kta76@sfu.ca>
2022-12-07 21:22:06 -0800

    selection operator implementation

---
+++ code/most_profit_selection.py
@@ -0,0 +1,14 @@
+from base_operator import SelectionOperator
+
+class MostProfitSelection(SelectionOperator):
+    def run(self) -> list:
+        """
+        Select a set of most fit individuals from input population, 
+        multiple different fitness scores from the fitness vector 
+        can be considered according to given priority
+        """
+        scores = {}
+        for state in self.optimizer.population:
+            scores[state['id']] = self.optimizer.fitness_function.evaluate(state)
+        self.optimizer.population.sort(key=lambda state: scores[state['id']]['profit'], reverse=True)
+        return self.optimizer.population[:self.optimizer.selection_factor]
